6f7e229d42a9e388aae92163bb3fde39afc2571f
Chris Williams <cwilliams@yext.com>
2018-11-02 15:55:43 -0400

    One simple trick to be good at FPS

--- keyboards/v60_type_r/keymaps/vimouse/keymap.c
+++ keyboards/v60_type_r/keymaps/vimouse/keymap.c
@@ -34,10 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT_all(
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  XXXXX, KC_BSPC, \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,     KC_BSLS, \
-        KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
+        KC_TAB,  KC_Q,    M(0),    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,     KC_BSLS, \
+        KC_LCTL,   M(1),    M(2),    M(3),    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT, \
         KC_LSFT, XXXXX,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, XXXXX,  \
-          MO(1), KC_LALT, KC_LGUI,               KC_SPC,                          KC_RGUI, KC_RALT, MO(1),  KC_RCTL),
+        MO(1), KC_LALT, KC_LGUI,               KC_SPC,                          KC_RGUI, KC_RALT, MO(1),  KC_RCTL),
     /* Keymap 1: FN Layer
      * ,-----------------------------------------------------------.
      * |`  | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|  Del  |
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_all(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  _____,  KC_DEL, \
         _____,   _____,   KC_UP,   _____,   _____,   _____,   _____,   _____,  KC_INS,   _____, KC_PSCR, KC_SLCK, KC_PAUS,          _____, \
-      KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT,   _____,   _____,   _____, KC_MPRV, KC_MNXT, KC_MPLY, KC_HOME, KC_PGUP,           _____,          \
+        KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT,   _____,   _____,   _____, KC_MPRV, KC_MNXT, KC_MPLY, KC_HOME, KC_PGUP,           _____,          \
         _____,   _____,   _____,   _____,   _____,   TG(2),   _____, KC_VOLD, KC_VOLU, KC_MUTE,  KC_END, KC_PGDN,           _____,  _____,  \
         _____,   _____,   _____,               _____,                                                      _____,   _____,  _____,  _____),
     /* Keymap 2: LED Modification Layer
@@ -81,18 +81,76 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM fn_actions[] = {
 };
 
+int nmback = 0;
+int nmforward = 0;
+int nmleft = 0;
+int nmright = 0;
+
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-  // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
-      }
+    // http://wiki.quakeworld.nu/Strafescript
+    switch(id) {
+        case 0: // W
+            if (record->event.pressed) {
+                register_code(KC_W);
+                nmforward = 1;
+                if (nmback > 0) {
+                    unregister_code(KC_S);
+                }
+            } else {
+                unregister_code(KC_W);
+                nmforward = 0;
+                if (nmback > 0) {
+                    register_code(KC_S);
+                }
+            }
+            break;
+        case 1: // A
+            if (record->event.pressed) {
+                register_code(KC_A);
+                nmleft = 1;
+                if (nmright > 0) {
+                    unregister_code(KC_D);
+                }
+            } else {
+                unregister_code(KC_A);
+                nmleft = 0;
+                if (nmright > 0) {
+                    register_code(KC_D);
+                }
+            }
+            break;
+        case 2: // S
+            if (record->event.pressed) {
+                register_code(KC_S);
+                nmback = 1;
+                if (nmforward > 0) {
+                    unregister_code(KC_W);
+                }
+            } else {
+                unregister_code(KC_S);
+                nmback = 0;
+                if (nmforward > 0) {
+                    register_code(KC_W);
+                }
+            }
+            break;
+        case 3: // D
+            if (record->event.pressed) {
+                register_code(KC_D);
+                nmright = 1;
+                if (nmleft > 0) {
+                    unregister_code(KC_A);
+                }
+            } else {
+                unregister_code(KC_D);
+                nmright = 0;
+                if (nmleft > 0) {
+                    register_code(KC_A);
+                }
+            }
+            break;
+    }
     return MACRO_NONE;
 };
 
