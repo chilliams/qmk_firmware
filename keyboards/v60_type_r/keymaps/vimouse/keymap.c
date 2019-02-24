bc79b531836b756d5666bef4be6312dcd908aad4
Chris Williams <chilliams@gmail.com>
2019-02-23 22:34:26 -0500

    Layout without leds

--- keyboards/v60_type_r/keymaps/vimouse/keymap.c
+++ keyboards/v60_type_r/keymaps/vimouse/keymap.c
@@ -55,26 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  _____,  KC_DEL, \
         _____,   _____,   KC_UP,   _____,   _____,   _____,   _____,   _____,  KC_INS,   _____, KC_PSCR, KC_SLCK, KC_PAUS,          _____, \
         KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT,   _____,   _____,   _____, KC_MPRV, KC_MNXT, KC_MPLY, KC_HOME, KC_PGUP,           _____,          \
-        _____,   _____,   _____,   _____,   _____,   TG(2),   _____, KC_VOLD, KC_VOLU, KC_MUTE,  KC_END, KC_PGDN,           _____,  _____,  \
-        _____,   _____,   _____,               _____,                                                      _____,   _____,  _____,  _____),
-    /* Keymap 2: LED Modification Layer
-     * ,-----------------------------------------------------------.
-     * |   |BLT|BLC|UGT|UGP|UGB|UGR|   |   |   |   |   |   |       |
-     * |-----------------------------------------------------------|
-     * |     |RD+|GN+|BL+|HU+|SA+|VA+|   |   |   |   |   |   |     |
-     * |-----------------------------------------------------------|
-     * |      |RD-|GN-|BL-|HU-|SA-|VA-|   |   |   |   |   |        |
-     * |-----------------------------------------------------------|
-     * |        |   |   |   |   |   |   |   |   |   |   |          |
-     * |-----------------------------------------------------------|
-     * |    |    |    |                        |    |    |    |    |
-     * `-----------------------------------------------------------'
-     */
-    [2] = LAYOUT_all(
-        TG(2), BL_TOGG, BL_STEP, RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R,   _____,   _____,   _____,   _____,   _____,   _____,  _____,  _____, \
-        _____, RGB_RI,  RGB_GI,  RGB_BI,  RGB_HUI, RGB_SAI, RGB_VAI,   _____,   _____,   _____,   _____,   _____,   _____,          _____, \
-        _____, RGB_RD,  RGB_GD,  RGB_BD,  RGB_HUD, RGB_SAD, RGB_VAD,   _____,   _____,   _____,   _____,   _____,           _____,          \
-        _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,   _____,           _____,  _____,  \
+        _____,   _____,   _____,   _____,   _____,   _____,   _____, KC_VOLD, KC_VOLU, KC_MUTE,  KC_END, KC_PGDN,           _____,  _____,  \
         _____,   _____,   _____,               _____,                                                      _____,   _____,  _____,  _____),
 };
 
@@ -156,14 +137,5 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 
 
 void led_set_user(uint8_t usb_led) {
-    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-        // output low
-        DDRE  |=  (1<<PE6);
-        PORTE &= ~(1<<PE6);
-    }
-    else {
-        // Hi-Z
-        DDRE  &= ~(1<<PE6);
-        PORTE &= ~(1<<PE6);
-    }
+
 }
